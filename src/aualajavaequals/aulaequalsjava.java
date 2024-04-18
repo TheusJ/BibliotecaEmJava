92370a975d163156048ed71ec3b3c730790f32c0
Matheus <mathjjc72@gmail.com>
2024-04-18 19:43:20 -0300

    @theus_js1 Signed-off-by:Matheus <mathjjc72@gmail.com>

---
+++ src/aualajavaequals/aulaequalsjava.java
@@ -0,0 +1,27 @@
+package aualajavaequals;
+
+import aualajavaequals.dominios.Livro;
+
+
+import java.util.Scanner;
+
+
+public class aulaequalsjava {
+
+    
+
+    
+    public static void main(String... args) {
+        Scanner texto = new Scanner(System.in);
+        Livro livro = new Livro();
+        
+        livro.menuPrincipal();
+        
+        
+        
+    }
+
+    
+    
+}
+
